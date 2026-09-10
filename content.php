e2869c2ee69558dca63bb007182259ffa2f1984e
rajesh-niceinteractive <150425123+rajesh-niceinteractive@users.noreply.github.com>
2026-09-10 18:10:25 +0530

    Refactor post item structure in content.php

--- content.php
+++ content.php
@@ -8,9 +8,8 @@
  */
 
 ?>
-
-<div class="post-item">
-    <div class="post-img">
+<article class="post-item post-<?php echo get_the_ID(); ?>">
+	<div class="post-img">
         <?php $img_url = (has_post_thumbnail()) ? get_the_post_thumbnail_url(get_the_ID(), 'full') : get_stylesheet_directory_uri().'/images/default-img.webp'; ?>
         <div class="blog-post-img"><a href="<?php the_permalink(); ?>"><img src="<?php echo $img_url; ?>" alt="<?php echo get_the_title(); ?>"></a></div>
     </div>
@@ -21,6 +20,6 @@
         </div>
         <h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
         <p><?php echo wp_trim_words(get_the_content(), 45); ?></p>
-            <div class="post-btn"><a href="<?php the_permalink(); ?>" class="blg-btn">Read More</a></div>
-        </div>
+        <div class="post-btn"><a href="<?php the_permalink(); ?>" class="blg-btn">Read More</a></div>
     </div>
+</article>
