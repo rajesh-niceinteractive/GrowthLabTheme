7f116f577c7ef728dcf7eaab841742314fa6a897
rajesh-niceinteractive <150425123+rajesh-niceinteractive@users.noreply.github.com>
2026-09-10 18:24:16 +0530

    Update content.php

--- content.php
+++ content.php
@@ -10,8 +10,21 @@
 ?>
 <article class="post-item post-<?php echo get_the_ID(); ?>">
 	<div class="post-img">
-        <?php $img_url = (has_post_thumbnail()) ? get_the_post_thumbnail_url(get_the_ID(), 'full') : get_stylesheet_directory_uri().'/images/default-img.webp'; ?>
-        <div class="blog-post-img"><a href="<?php the_permalink(); ?>"><img src="<?php echo $img_url; ?>" alt="<?php echo get_the_title(); ?>"></a></div>
+        <?php $img_url = (has_post_thumbnail()) ? get_the_post_thumbnail_url(get_the_ID(), 'full') : get_stylesheet_directory_uri().'/images/default-img.webp';
+if (!has_post_thumbnail()) {
+            $size = getimagesize(get_stylesheet_directory() . '/images/blog-post-default-img.webp');
+            $img[1] = $size[0];
+            $img[2] = $size[1];
+        } else {
+            $size = $img;
+            $img[1] = $size[0];
+            $img[2] = $size[1];
+        } ?>
+        <div class="blog-post-img">
+			<a href="<?php the_permalink(); ?>">
+				<img src="<?php echo esc_url($img[0]); ?>" width="<?php echo $img[1]; ?>" height="<?php echo $img[2]; ?>" alt="<?php echo esc_attr(get_the_title()); ?>">
+			</a>
+		</div>
     </div>
     <div class="post-cnt">
 		<div class="post-meta">
